00daa1efc5d7628b2df994806f73226cabdff297
Кирило <Korsun.K.Y@nmu.one>
2026-09-12 18:29:34 +0300

    Change greeting in main

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello from MAIN and FEATURE!");
+        Console.WriteLine("Hello from MAIN!");
         GreetUser("User");
     }
 
